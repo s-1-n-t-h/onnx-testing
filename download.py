6cf827c2183e9279056a2f4f1ac8b269fcaecc8a
B V S YASWANTH <83590022+s-1-n-t-h@users.noreply.github.com>
2023-03-10 19:30:25 +0530

    Update download.py

--- download.py
+++ download.py
@@ -3,16 +3,17 @@
 
 # In this example: A Huggingface BERT model
 
-from transformers import AutoTokenizer
-from optimum.pipelines import pipeline
+from transformers import AutoTokenizer, pipeline
 from optimum.onnxruntime import ORTModelForSeq2SeqLM
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    model_name = 's-1-n-t-h/bart-cnn-optimised'
+   
     #model = ORTModelForSeq2SeqLM.from_pretrained(model_name,from_transformers=True,use_auth_token='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')
     #tokenizer = AutoTokenizer.from_pretrained(model_name,use_auth_token='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')
-    pipeline('summarization', model=model_name,use_auth_token='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',accelerator='ort')
+    model = ORTModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn", from_transformers=True)
+    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+    pipeline("summarization", model=model, tokenizer=tokenizer,accelarator='ort')
 
 if __name__ == "__main__":
     download_model()
