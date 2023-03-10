6427b2f8d402326436bdd49e705c58329dc5099d
B V S YASWANTH <83590022+s-1-n-t-h@users.noreply.github.com>
2023-03-10 13:30:04 +0530

    Update download.py

--- download.py
+++ download.py
@@ -10,9 +10,10 @@ from optimum.onnxruntime import ORTModelForSeq2SeqLM
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     model_name = 's-1-n-t-h/bart-cnn-optimised'
-    model = ORTModelForSeq2SeqLM.from_pretrained(model_name,from_transformers=True,use_auth_token='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')
-    tokenizer = AutoTokenizer.from_pretrained(model_name,use_auth_token='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')
-    #pipeline('summarization', model="facebook/bart-large-cnn", framework='pt',use_auth_token='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')
+    #model = ORTModelForSeq2SeqLM.from_pretrained(model_name,from_transformers=True,use_auth_token='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')
+    #tokenizer = AutoTokenizer.from_pretrained(model_name,use_auth_token='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')
+    pipeline('summarization', model=model_name,
+                     device=device, use_auth_token='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',accelerator='ort'
 
 if __name__ == "__main__":
     download_model()
